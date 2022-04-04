1e811884e7e3206af1abcb1bd4842832a763cf79
davidegaribaldi <100833454+davidegaribaldi@users.noreply.github.com>
2022-04-04 13:19:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n = 0, a = 1, b = 1,c=0;
+    cin >> n;
+    if(n>=1)
+    {
+        cout << a << endl;
+        cout << b << endl;
+        while(n>=(a+b))
+        {
+        c=a+b;
+        a=b;
+        b=c;
+        cout<<c<<endl;
+        }
+    }
+
+    return 0;
 }
+
